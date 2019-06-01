a7f38ae257622f1bebf30a24e475984592ac550a
Afterburner <rockey1024@icloud.com>
2019-06-01 11:01:20 +0800

    I2Helper single instance mode

--- I2Helper/Program.cs
+++ I2Helper/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,28 @@ namespace I2Helper
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            string guid = GetGuid();
+            using (Mutex mutex = new Mutex(false, "Global\\" + guid))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("程序已运行");
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+
+        
+        }
+
+        private static string GetGuid()
+        {
+            var assembly = typeof(Program).Assembly;
+            var attribute = (GuidAttribute)assembly.GetCustomAttributes(typeof(GuidAttribute), true)[0];
+            return attribute.Value;
         }
     }
 }
